b30edc4968f52d4612574e36978d35c372a07783
Sinead McCaffery <sineadmccaffery@Sineads-MacBook-Air.local>
2022-11-29 17:48:42 -0800

    party page

--- client/src/pages/Party.js
+++ client/src/pages/Party.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import MemberBox from '../components/MemberBox';
+import Logo from '../components/LogoAndWebsite';
+import styles from "../components/WaitingText.module.css";
 
 const Party = () => {
     return (
         <div>
-            
+            <Logo />
+            <MemberBox/>
+            <p className = {styles['text']}>Waiting for host to start...</p>
         </div>
     );
 };
