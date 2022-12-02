21100ade0a357014756cceb3dc7d0e2d904f0e25
Ronnie Beggs <ronald.beggs@berkeley.edu>
2022-12-01 20:34:32 -0800

    Updated host page to show party members and party code.

--- client/src/pages/HostParty.js
+++ client/src/pages/HostParty.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import StartMatchingButton from '../components/StartMatchingButton';
 import Logo from '../components/LogoAndWebsite';
+import siteStyles from '../components/LogoAndWebsite.module.css';
 import MemberBox from '../components/MemberBox';
 import {Routes, Route, useNavigate, useNavigation} from 'react-router-dom';
+import axios from 'axios';
 
 const HostParty = () => {
     const navigate = useNavigate();
+
+    var [memberList, updateList] = useState([]);
+    var [partyId, updateId] = useState("");
+
+    const populateList = () => {
+        axios
+            .get('http://localhost:9000/party/info', { params: { nickname: "ronnie5" }})
+            .then((data) => {
+                updateId(data.data.partyId);
+                updateList(data.data.partyMembers);
+            })
+            .catch((error) => console.log(error.response.data));
+    };
+
+    useEffect(() => {
+        populateList();
+    }, [])
 
     const navigateToSwiping = () => {
         navigate('/swiping');
@@ -14,7 +33,8 @@ const HostParty = () => {
     return (
         <div>
             <Logo />
-            <MemberBox/>
+            <h2 className = {siteStyles['site']}>Code: {partyId}</h2>
+            <MemberBox memberList={memberList}/>
             <StartMatchingButton onClick={navigateToSwiping}>Start Matching</StartMatchingButton>
         </div>
     );
--- client/src/pages/Party.js
+++ client/src/pages/Party.js
@@ -1,17 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import MemberBox from '../components/MemberBox';
 import Logo from '../components/LogoAndWebsite';
+import siteStyles from '../components/LogoAndWebsite.module.css';
 import styles from "../components/WaitingText.module.css";
 import axios from 'axios';
 
 const Party = (props) => {
 
     var [memberList, updateList] = useState([]);
+    var [partyId, updateId] = useState("");
 
     const populateList = () => {
         axios
             .get('http://localhost:9000/party/info', { params: { nickname: "ronnie5" }})
             .then((data) => {
+                updateId(data.data.partyId);
                 updateList(data.data.partyMembers);
             })
             .catch((error) => console.log(error.response.data));
@@ -24,6 +27,7 @@ const Party = (props) => {
     return (
         <div>
             <Logo />
+            <h2 className = {siteStyles['site']}>Code: {partyId}</h2>
             <MemberBox memberList={memberList}/>
             <p className = {styles['text']}>Waiting for host to start...</p>
         </div>
